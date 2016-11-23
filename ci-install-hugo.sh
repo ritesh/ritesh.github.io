4a961cb89280a0c6a3b2f322d1f5e344297d5d0e
Ritesh Sinha <ritesh@cigital.com>
2016-11-23 17:17:39 +0000

    Fix hugo path

--- ci-install-hugo.sh
+++ ci-install-hugo.sh
@@ -5,7 +5,7 @@ set -e
 
 # Install Hugo if not already cached or upgrade an old version.
 if [ ! -e $CIRCLE_BUILD_DIR/bin/hugo ] || ! [[ `hugo version` =~ v${HUGO_VERSION} ]]; then
-  wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux_amd64.tar.gz
-  tar xvzf hugo_${HUGO_VERSION}_linux_amd64.tar.gz
-  cp hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 $CIRCLE_BUILD_DIR/bin/hugo
+  wget https://github.com/spf13/hugo/releases/download/v0.17/hugo_0.17_Linux-64bit.tar.gz 
+  tar xvzf hugo_0.17_Linux-64bit.tar.gz
+  cp hugo_0.17_linux_amd64/hugo_0.17_linux_amd64 $CIRCLE_BUILD_DIR/bin/hugo
 fi
