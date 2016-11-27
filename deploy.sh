896376eac39208a9f284e26d9aa38ccc6ed0bb60
Ritesh Sinha <r@rsinha.org>
2016-11-27 22:11:58 +0000

    rebuilding site Sun 27 Nov 2016 22:11:58 GMT

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 git subtree pull --prefix=public git@github.com:ritesh/ritesh.github.io.git master --squash
+git remote add -f ritesh-github-io git@github.com:ritesh/ritesh.github.io.git master 
+
 hugo 
 
 # Commit changes.
@@ -18,4 +20,5 @@ git commit -m "$msg"
 
 # Come Back
 echo -e "\033[0;32mPushing subtree containing blog posts...\033[0m"
-git subtree push --prefix=public git@github.com:ritesh/ritesh.github.io.git master
+git push origin `git subtree split --prefix=public ritesh-github-io`:master --force
+# git subtree push --prefix=public git@github.com:ritesh/ritesh.github.io.git master
